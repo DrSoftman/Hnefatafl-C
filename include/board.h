d213380cf00586c77b40755082e9edf130f800d0
DrSoftman <168275864+DrSoftman@users.noreply.github.com>
2025-02-22 23:09:19 +0100

    Update board.h

--- include/board.h
+++ include/board.h
@@ -2,6 +2,15 @@
 #ifndef BOARD_H
 #define BOARD_H
 
+#define EMPTY 0
+#define ATTACKER 1
+#define DEFENDER 2
+#define KING 3
+
 #include <stdio.h>
+
+void initBoard(char board[11][11]) {
+  
+}
 
 #endif // BOARD_H
