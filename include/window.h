bc00887e968bee77a2ee1037dfd5aea2001024e4
Wickslynx <167521638+Wickslynx@users.noreply.github.com>
2025-02-23 14:06:30 +0100

    Made the strucutrfe

--- include/window.h
+++ include/window.h
@@ -1,9 +1,35 @@
 #ifndef WINDOW_H
 #define WINDOW_H
 
+
+/*Wicks speaking (Creator of RoofNut)
+
+I would upgrade the RoofNut Version and then instead use the internal widgets, the current as of writing supported internal widgets are:
+
+  RoofNutCircle();
+  RoofNutTriangle();
+  RoofNutRectangle();
+
+  I will add more in the future, good luck!
+*/
+
+
+#define ROOFNUT_EXPERIMENTAL_INTERNAL_GUI
+#define ROOFNUT_IMPLEMENENTATION
 #include <roofnut.h>
 #include <stdio.h>
 
+void RoofNutRender() {
+    
+}
 
+
+DrawBoard() {
+
+}
+
+MainGuiLoop() {
+  
+}
 
 #endif // WINDOW_H
